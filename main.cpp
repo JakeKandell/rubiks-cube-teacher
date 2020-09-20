3e8a15db5758fee48b82a3fd38afc0aacf6f4385
Jake Kandell <jakekand@usc.edu>
2020-09-20 05:55:53 -0400

    tested image capture

--- main.cpp
+++ main.cpp
@@ -5,19 +5,36 @@ using namespace cv;
 
 int main(int argc, char** argv)
 {
-    VideoCapture cap(2);
+    VideoCapture cap(3);
     // open the default camera, use something different from 0 otherwise;
     // Check VideoCapture documentation.
-    if(!cap.open(2))
+    if(!cap.open(3))
         return 0;
+
+    Mat frame;
     while (1)
     {
-          Mat frame;
           cap >> frame;
           if( frame.empty() ) break; // end of video stream
           imshow("this is you, smile! :)", frame);
           if( waitKey(10) == 27 ) break; // stop capturing by pressing ESC
     }
+
+    destroyAllWindows();
+
+    while (1) {
+        imshow("Captured Image", frame);
+        if( waitKey(10) == 27 ) break; // stop capturing by pressing ESC
+    }
+
+    Mat hsv;
+    cvtColor(frame, hsv, COLOR_BGR2HSV);
+
+    while (1) {
+        imshow("Color Shifted", hsv);
+        if( waitKey(10) == 27 ) break; // stop capturing by pressing ESC
+    }
+
     // the camera will be closed automatically upon exit
     // cap.close();
     return 0;
