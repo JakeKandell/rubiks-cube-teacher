5434513d209fe04a584718d7a3f612fdfaa5c4db
Jake Kandell <jakekand@usc.edu>
2020-10-01 04:39:57 -0400

    added thresholding test

--- main.cpp
+++ main.cpp
@@ -3,6 +3,43 @@
 
 using namespace cv;
 
+const int max_value_H = 360/2;
+const int max_value = 255;
+const String window_capture_name = "Video Capture";
+const String window_detection_name = "Object Detection";
+int low_H = 0, low_S = 0, low_V = 0;
+int high_H = max_value_H, high_S = max_value, high_V = max_value;
+static void on_low_H_thresh_trackbar(int, void *)
+{
+    low_H = min(high_H-1, low_H);
+    setTrackbarPos("Low H", window_detection_name, low_H);
+}
+static void on_high_H_thresh_trackbar(int, void *)
+{
+    high_H = max(high_H, low_H+1);
+    setTrackbarPos("High H", window_detection_name, high_H);
+}
+static void on_low_S_thresh_trackbar(int, void *)
+{
+    low_S = min(high_S-1, low_S);
+    setTrackbarPos("Low S", window_detection_name, low_S);
+}
+static void on_high_S_thresh_trackbar(int, void *)
+{
+    high_S = max(high_S, low_S+1);
+    setTrackbarPos("High S", window_detection_name, high_S);
+}
+static void on_low_V_thresh_trackbar(int, void *)
+{
+    low_V = min(high_V-1, low_V);
+    setTrackbarPos("Low V", window_detection_name, low_V);
+}
+static void on_high_V_thresh_trackbar(int, void *)
+{
+    high_V = max(high_V, low_V+1);
+    setTrackbarPos("High V", window_detection_name, high_V);
+}
+
 int main(int argc, char** argv)
 {
     VideoCapture cap(2);
@@ -11,141 +48,50 @@ int main(int argc, char** argv)
     if(!cap.open(2))
         return 0;
 
-
+    namedWindow(window_capture_name);
+    namedWindow(window_detection_name);
+    // Trackbars to set thresholds for HSV values
+    createTrackbar("Low H", window_detection_name, &low_H, max_value_H, on_low_H_thresh_trackbar);
+    createTrackbar("High H", window_detection_name, &high_H, max_value_H, on_high_H_thresh_trackbar);
+    createTrackbar("Low S", window_detection_name, &low_S, max_value, on_low_S_thresh_trackbar);
+    createTrackbar("High S", window_detection_name, &high_S, max_value, on_high_S_thresh_trackbar);
+    createTrackbar("Low V", window_detection_name, &low_V, max_value, on_low_V_thresh_trackbar);
+    createTrackbar("High V", window_detection_name, &high_V, max_value, on_high_V_thresh_trackbar);
 
     Mat frame;
-    while (1)
-    {
-          cap >> frame;
-          if( frame.empty() ) break; // end of video stream
-          imshow("this is you, smile! :)", frame);
-          if( waitKey(10) == 27 ) break; // stop capturing by pressing ESC
+    // while (1)
+    // {
+    //       cap >> frame;
+    //       if( frame.empty() ) break; // end of video stream
+    //       imshow("this is you, smile! :)", frame);
+    //       if( waitKey(10) == 27 ) break; // stop capturing by pressing ESC
+    // }
+    //
+    // destroyAllWindows();
+
+    Mat frame_hsv;
+    Mat frame_threshold;
+
+    while (true) {
+        cap >> frame;
+        if(frame.empty())
+        {
+            break;
+        }
+        // Convert from BGR to HSV colorspace
+        cvtColor(frame, frame_hsv, COLOR_BGR2HSV);
+        // Detect the object based on HSV Range Values
+        inRange(frame_hsv, Scalar(low_H, low_S, low_V), Scalar(high_H, high_S, high_V), frame_threshold);
+        // Show the frames
+        imshow(window_capture_name, frame);
+        imshow(window_detection_name, frame_threshold);
+        char key = (char) waitKey(30);
+        if (key == 'q' || key == 27)
+        {
+            break;
+        }
     }
-
-
-
-
-    destroyAllWindows();
-
-    while (1) {
-        imshow("Captured Image", frame);
-        if( waitKey(10) == 27 ) break; // stop capturing by pressing ESC
-    }
-
-    namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
-
-    int iLowH = 0;
-    int iHighH = 179;
-
-    int iLowS = 0;
-    int iHighS = 255;
-
-    int iLowV = 0;
-    int iHighV = 255;
-
-    //Create trackbars in "Control" window
-    cvCreateTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
-    cvCreateTrackbar("HighH", "Control", &iHighH, 179);
-
-    cvCreateTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
-    cvCreateTrackbar("HighS", "Control", &iHighS, 255);
-
-    cvCreateTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
-    cvCreateTrackbar("HighV", "Control", &iHighV, 255);
-
-    Mat hsv;
-    cvtColor(frame, hsv, COLOR_BGR2HSV);
-
-    while (1) {
-        imshow("Control", hsv);
-        imshow("Color Shifted", hsv);
-        if( waitKey(10) == 27 ) break; // stop capturing by pressing ESC
-    }
-
     // the camera will be closed automatically upon exit
     // cap.close();
     return 0;
 }
-
-
-/*
-Thresholds:
-
-COUCH:
-
-Red:
-121, 1, 0
-0.5, 100, 47
-
-Orange:
-196, 4, 0
-1.2, 100, 77
-
-Green:
-72, 132, 62
-111, 53, 52
-
-Blue:
-16, 19, 64
-236, 75, 25
-
-White:
-188, 171, 143
-37, 24, 74
-
-Yellow:
-205, 189, 18
-
-BLANK WALL:
-
-Red:
-190, 0, 0
-0, 100, 75
-
-Orange:
-255, 90, 1
-21, 100, 100
-
-Green:
-139, 209, 120
-107, 43, 82
-
-Blue:
-21, 48, 139
-226, 85, 55
-
-White:
-255, 255, 235
-60, 9, 100
-
-Yellow
-254, 255, 115
-60, 55, 100
-
-COMPUTER:
-
-Red:
-174, 0, 0
-0, 100, 68
-
-Orange:
-255, 19, 0
-5, 100, 100
-
-Green:
-130, 191, 88
-96, 54, 75
-
-Blue:
-28, 36, 101
-233, 72, 40
-
-White:
-254, 241, 199
-35, 22, 100
-
-Yellow:
-254, 250, 18
-59, 93, 100
-
-*/
