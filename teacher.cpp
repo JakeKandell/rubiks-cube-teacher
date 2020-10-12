da7dbfa9543793f50ffd3c26b60723817e693ca8
Jake Kandell <jakekand@usc.edu>
2020-10-12 06:06:58 -0400

    moved some thresholding functionality to colors.cpp

--- teacher.cpp
+++ teacher.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include "opencv2/opencv.hpp"
 #include "iostream"
+#include "colors.h"
 
 using namespace cv;
 using namespace std;
@@ -50,8 +51,8 @@ int main(int argc, char** argv)
     if(!cap.open(2))
         return 0;
 
-    namedWindow(window_capture_name);
     namedWindow(window_detection_name);
+    namedWindow("Video Capture");
     // Trackbars to set thresholds for HSV values
     createTrackbar("Low H", window_detection_name, &low_H, max_value_H, on_low_H_thresh_trackbar);
     createTrackbar("High H", window_detection_name, &high_H, max_value_H, on_high_H_thresh_trackbar);
@@ -70,14 +71,14 @@ int main(int argc, char** argv)
         {
             break;
         }
-        // Convert from BGR to HSV colorspace
+        // // Convert from BGR to HSV colorspace
         cvtColor(frame, frame_hsv, COLOR_BGR2HSV);
-        // Detect the object based on HSV Range Values
-        // inRange(frame_hsv, Scalar(low_H, low_S, low_V), Scalar(high_H, high_S, high_V), frame_threshold);
-        inRange(frame_hsv, Scalar(0, 0, 237), Scalar(40, 56, 255), frame_threshold);
-        // Show the frames
-        imshow(window_capture_name, frame);
+        // // Detect the object based on HSV Range Values
+        inRange(frame_hsv, Scalar(low_H, low_S, low_V), Scalar(high_H, high_S, high_V), frame_threshold);
+        //inRange(frame_hsv, Scalar(0, 220, 160), Scalar(6, 255, 215), frame_threshold);
+        // // Show the frames
         imshow(window_detection_name, frame_threshold);
+        imshow("Video Capture", frame);
         char key = (char) waitKey(30);
         if (key == 'q' || key == 27)
         {
@@ -86,58 +87,61 @@ int main(int argc, char** argv)
     }
 
     destroyAllWindows();
+    thresholdColors(frame);
 
-    while (true) {
-        imshow("Saved Image", frame);
-        imshow("Saved Threshold", frame_threshold);
+    // destroyAllWindows();
+    //
+    // while (true) {
+    //     imshow("Saved Image", frame);
+    //     imshow("Saved Threshold", frame_threshold);
+    //
+    //     char key = (char) waitKey(30);
+    //     if (key == 'q' || key == 27)
+    //     {
+    //         break;
+    //     }
+    // }
 
-        char key = (char) waitKey(30);
-        if (key == 'q' || key == 27)
-        {
-            break;
-        }
-    }
-
-    Mat new_threshold;
-    Mat kernel = getStructuringElement(0, Size(3, 3), Point(-1, -1));
-    Mat kernel2 = getStructuringElement(0, Size(5, 5), Point(-1, -1));
-
-    morphologyEx(frame_threshold, new_threshold, 2, kernel);
-    morphologyEx(new_threshold, new_threshold, 3, kernel2);
-
-    while (true) {
-        imshow("Updated Threshold", new_threshold);
-
-        char key = (char) waitKey(30);
-        if (key == 'q' || key == 27)
-        {
-            break;
-        }
-    }
-
-    Mat canny_output;
-    Canny(new_threshold, canny_output, 100, 200);
-
-    vector<vector<Point>> contours;
-    vector<Vec4i> hierarchy;
-    findContours(canny_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
-
-    Mat frame_countours = frame;
-
-    Scalar color = Scalar(rand()&255, rand()&255, rand()&255);
-    for( size_t i = 0; i< contours.size(); i++ ) {
-        drawContours(frame_countours, contours, (int)i, color, 1, LINE_8, hierarchy, 0);
-    }
-
-    while (true) {
-        imshow("With Countours", frame_countours);
-
-        char key = (char) waitKey(30);
-        if (key == 'q' || key == 27)
-        {
-            break;
-        }
-    }
+    // Mat new_threshold;
+    // Mat kernel = getStructuringElement(0, Size(3, 3), Point(-1, -1));
+    // Mat kernel2 = getStructuringElement(0, Size(5, 5), Point(-1, -1));
+    //
+    // morphologyEx(frame_threshold, new_threshold, 2, kernel);
+    // morphologyEx(new_threshold, new_threshold, 3, kernel2);
+    //
+    // while (true) {
+    //     imshow("Updated Threshold", new_threshold);
+    //
+    //     char key = (char) waitKey(30);
+    //     if (key == 'q' || key == 27)
+    //     {
+    //         break;
+    //     }
+    // }
+    //
+    // Mat canny_output;
+    // Canny(new_threshold, canny_output, 100, 200);
+    //
+    // vector<vector<Point>> contours;
+    // vector<Vec4i> hierarchy;
+    // findContours(canny_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
+    //
+    // Mat frame_countours = frame;
+    //
+    // Scalar color = Scalar(rand()&255, rand()&255, rand()&255);
+    // for( size_t i = 0; i< contours.size(); i++ ) {
+    //     drawContours(frame_countours, contours, (int)i, color, 1, LINE_8, hierarchy, 0);
+    // }
+    //
+    // while (true) {
+    //     imshow("With Countours", frame_countours);
+    //
+    //     char key = (char) waitKey(30);
+    //     if (key == 'q' || key == 27)
+    //     {
+    //         break;
+    //     }
+    // }
     // the camera will be closed automatically upon exit
     // cap.close();
     return 0;
