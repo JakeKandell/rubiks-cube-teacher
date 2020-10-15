091ff063850d1a402e316d213a33e53df11fbcb3
Jake Kandell <jakekand@usc.edu>
2020-10-15 05:07:09 -0400

    added bounding rectangle identification

--- colors.cpp
+++ colors.cpp
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <vector>
+#include <iostream>
 
 using namespace cv;
 using namespace std;
 
-void thresholdOneColor(Mat& frameHSV, Mat& frameCountours, Scalar lowThresh, Scalar highThresh) {
+void thresholdOneColor(Mat& frameHSV, Mat& frameContours, Scalar lowThresh, Scalar highThresh, vector<vector<Point>>& allContours, vector<Vec4i>& hierarchy) {
 
     Mat frameThreshold;
 
@@ -15,7 +16,7 @@ void thresholdOneColor(Mat& frameHSV, Mat& frameCountours, Scalar lowThresh, Sca
 
     // shows orig image with threshold
     while (true) {
-        imshow("Saved Image", frameCountours);
+        imshow("Saved Image", frameContours);
         imshow("Saved Threshold", frameThreshold);
 
         char key = (char) waitKey(30);
@@ -26,7 +27,7 @@ void thresholdOneColor(Mat& frameHSV, Mat& frameCountours, Scalar lowThresh, Sca
     }
 
     // kernels used for morphology operations
-    Mat kernelOpen = getStructuringElement(0, Size(3, 3), Point(-1, -1));
+    Mat kernelOpen = getStructuringElement(0, Size(5, 5), Point(-1, -1));
     Mat kernelClose = getStructuringElement(0, Size(5, 5), Point(-1, -1));
 
     // opening operation to remove small noise
@@ -48,20 +49,20 @@ void thresholdOneColor(Mat& frameHSV, Mat& frameCountours, Scalar lowThresh, Sca
     Mat cannyOutput;
     Canny(frameThreshold, cannyOutput, 100, 200);
 
-    vector<vector<Point>> contours;
-    vector<Vec4i> hierarchy;
+    vector<vector<Point>> tempContours;
+    vector<Vec4i> tempHierarchy;
     // find countours of threshold
-    findContours(cannyOutput, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
+    findContours(cannyOutput, tempContours, tempHierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
 
     // picks a random color to draw countours
     Scalar color = Scalar(rand()&255, rand()&255, rand()&255);
     // draws countours onto image
-    for(size_t i = 0; i< contours.size(); i++) {
-        drawContours(frameCountours, contours, (int)i, color, 2, LINE_8, hierarchy, 0);
+    for(size_t i = 0; i< tempContours.size(); i++) {
+        drawContours(frameContours, tempContours, (int)i, color, 2, LINE_8, tempHierarchy, 0);
     }
 
     while (true) {
-        imshow("With Countours", frameCountours);
+        imshow("With Countours", frameContours);
 
         char key = (char) waitKey(30);
         if (key == 'q' || key == 27)
@@ -72,6 +73,8 @@ void thresholdOneColor(Mat& frameHSV, Mat& frameCountours, Scalar lowThresh, Sca
 
     destroyAllWindows();
 
+    allContours.insert(allContours.end(), tempContours.begin(), tempContours.end());
+
 }
 
 
@@ -79,24 +82,41 @@ void thresholdColors(Mat frame) {
 
     srand(time(NULL));
 
-    Mat frameCountours = frame;
+    Mat frameContours = frame;
     Mat frameHSV;
+
+    vector<vector<Point>> allContours;
+    vector<Vec4i> hierarchy;
 
     // Convert from BGR to HSV colorspace
     cvtColor(frame, frameHSV, COLOR_BGR2HSV);
 
     // thresholds red
-    thresholdOneColor(frameHSV, frameCountours, Scalar(0, 238, 163), Scalar(4, 255, 204));
+    thresholdOneColor(frameHSV, frameContours, Scalar(0, 238, 163), Scalar(4, 255, 204), allContours, hierarchy);
     // thresholds orange
-    thresholdOneColor(frameHSV, frameCountours, Scalar(4, 183, 220), Scalar(23, 255, 255));
+    thresholdOneColor(frameHSV, frameContours, Scalar(4, 183, 220), Scalar(23, 255, 255), allContours, hierarchy);
     // thresholds yellow
-    thresholdOneColor(frameHSV, frameCountours, Scalar(21, 90, 212), Scalar(40, 206, 255));
+    thresholdOneColor(frameHSV, frameContours, Scalar(21, 90, 212), Scalar(40, 206, 255), allContours, hierarchy);
     // thresholds green
-    thresholdOneColor(frameHSV, frameCountours, Scalar(43, 61, 156), Scalar(68, 125, 238));
+    thresholdOneColor(frameHSV, frameContours, Scalar(43, 61, 156), Scalar(68, 125, 238), allContours, hierarchy);
     // thresholds blue
-    thresholdOneColor(frameHSV, frameCountours, Scalar(89, 105, 103), Scalar(129, 235, 191));
+    thresholdOneColor(frameHSV, frameContours, Scalar(89, 105, 103), Scalar(129, 235, 191), allContours, hierarchy);
     // thresholds white
-    thresholdOneColor(frameHSV, frameCountours, Scalar(0, 0, 237), Scalar(40, 56, 255));
+    thresholdOneColor(frameHSV, frameContours, Scalar(0, 0, 237), Scalar(40, 56, 255), allContours, hierarchy);
+
+    cout << "Contours Size: " << allContours.size()/2 << endl;
+
+    for (unsigned int i=0; i < allContours.size(); i += 2) {
+        Rect boundingRectangle = boundingRect(allContours[i]);
+
+        cout << boundingRectangle << endl;
+
+        Point topLeft = boundingRectangle.tl();
+        cout << topLeft << endl;
+
+        Size rectSize = boundingRectangle.size();
+        cout << rectSize << endl;
+    }
 
 
 
--- colors.h
+++ colors.h
@@ -1,5 +1,6 @@
 #include "opencv2/opencv.hpp"
+#include <vector>
 
-void thresholdOneColor(cv::Mat& frameHSV, cv::Mat& frameCountours, cv::Scalar lowThresh, cv::Scalar highThresh);
+void thresholdOneColor(cv::Mat& frameHSV, cv::Mat& frameContours, cv::Scalar lowThresh, cv::Scalar highThresh,  std::vector<std::vector<cv::Point>>& contours, std::vector<cv::Vec4i>& hierarchy);
 
 void thresholdColors(cv::Mat frame);
